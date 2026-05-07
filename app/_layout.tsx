48ee05a00f480d4f98fd43b4bb6c41fcc4d0609e
Hyperion147 <hypercode9913@gmail.com>
2026-05-07 18:21:24 +0530

    feat(styling): add Tailwind CSS and shadcn/ui component setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import './global.css';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -19,6 +21,7 @@ export default function RootLayout() {
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
+      <PortalHost />
     </ThemeProvider>
   );
 }
